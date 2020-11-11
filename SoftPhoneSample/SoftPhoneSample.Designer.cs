51ab08534dc83aa383471fb992fe2fe0478a9cc7
Ko.Lee <ko.lee@quintet.co.kr>
2020-11-11 13:35:46 +0900

    init commit

---
+++ SoftPhoneSample/SoftPhoneSample.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace SoftPhoneSample
+{
+    partial class SoftPhoneSample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SoftPhoneSample));
+            this.iCapi = new AxICAPILib.AxICAPI();
+            this.lbl_UserId = new System.Windows.Forms.Label();
+            this.txt_UserId = new System.Windows.Forms.TextBox();
+            this.txt_UserPw = new System.Windows.Forms.TextBox();
+            this.lbl_UserPw = new System.Windows.Forms.Label();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_AnswerCallNumber = new System.Windows.Forms.TextBox();
+            this.btn_AnswerCall = new System.Windows.Forms.Button();
+            this.btn_ClearCall = new System.Windows.Forms.Button();
+            this.txt_AgentDn = new System.Windows.Forms.TextBox();
+            this.lbl_AgentDn = new System.Windows.Forms.Label();
+            this.txt_DestDn = new System.Windows.Forms.TextBox();
+            this.lbl_DestDn = new System.Windows.Forms.Label();
+            this.btn_MakeCall = new System.Windows.Forms.Button();
+            this.lbl_LogLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize) (this.iCapi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // iCapi
+            // 
+            this.iCapi.Enabled = true;
+            this.iCapi.Location = new System.Drawing.Point(404, 280);
+            this.iCapi.Margin = new System.Windows.Forms.Padding(4);
+            this.iCapi.Name = "iCapi";
+            this.iCapi.OcxState = ((System.Windows.Forms.AxHost.State) (resources.GetObject("iCapi.OcxState")));
+            this.iCapi.Size = new System.Drawing.Size(150, 75);
+            this.iCapi.TabIndex = 0;
+            this.iCapi.Visible = false;
+            this.iCapi.Ringing += new AxICAPILib._DICAPIEvents_RingingEventHandler(this.iCapi_Ringing);
+            this.iCapi.AgentLogin += new AxICAPILib._DICAPIEvents_AgentLoginEventHandler(this.iCapi_AgentLogin);
+            this.iCapi.ResponseAnswerCall += new AxICAPILib._DICAPIEvents_ResponseAnswerCallEventHandler(this.iCapi_ResponseAnswerCall);
+            this.iCapi.ResponseAgentLogin += new AxICAPILib._DICAPIEvents_ResponseAgentLoginEventHandler(this.iCapi_ResponseAgentLogin);
+            // 
+            // lbl_UserId
+            // 
+            this.lbl_UserId.AutoSize = true;
+            this.lbl_UserId.Location = new System.Drawing.Point(31, 20);
+            this.lbl_UserId.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_UserId.Name = "lbl_UserId";
+            this.lbl_UserId.Size = new System.Drawing.Size(62, 18);
+            this.lbl_UserId.TabIndex = 1;
+            this.lbl_UserId.Text = "아이디";
+            // 
+            // txt_UserId
+            // 
+            this.txt_UserId.Location = new System.Drawing.Point(135, 16);
+            this.txt_UserId.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_UserId.Name = "txt_UserId";
+            this.txt_UserId.Size = new System.Drawing.Size(284, 28);
+            this.txt_UserId.TabIndex = 2;
+            // 
+            // txt_UserPw
+            // 
+            this.txt_UserPw.Location = new System.Drawing.Point(135, 49);
+            this.txt_UserPw.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_UserPw.Name = "txt_UserPw";
+            this.txt_UserPw.PasswordChar = '*';
+            this.txt_UserPw.Size = new System.Drawing.Size(284, 28);
+            this.txt_UserPw.TabIndex = 4;
+            // 
+            // lbl_UserPw
+            // 
+            this.lbl_UserPw.AutoSize = true;
+            this.lbl_UserPw.Location = new System.Drawing.Point(17, 52);
+            this.lbl_UserPw.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_UserPw.Name = "lbl_UserPw";
+            this.lbl_UserPw.Size = new System.Drawing.Size(80, 18);
+            this.lbl_UserPw.TabIndex = 3;
+            this.lbl_UserPw.Text = "비밀번호";
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Location = new System.Drawing.Point(429, 16);
+            this.btn_Login.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(107, 99);
+            this.btn_Login.TabIndex = 5;
+            this.btn_Login.Text = "Login";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(10, 142);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "전화번호";
+            // 
+            // txt_AnswerCallNumber
+            // 
+            this.txt_AnswerCallNumber.Location = new System.Drawing.Point(135, 139);
+            this.txt_AnswerCallNumber.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_AnswerCallNumber.MaxLength = 11;
+            this.txt_AnswerCallNumber.Name = "txt_AnswerCallNumber";
+            this.txt_AnswerCallNumber.ReadOnly = true;
+            this.txt_AnswerCallNumber.Size = new System.Drawing.Size(284, 28);
+            this.txt_AnswerCallNumber.TabIndex = 7;
+            // 
+            // btn_AnswerCall
+            // 
+            this.btn_AnswerCall.Location = new System.Drawing.Point(429, 139);
+            this.btn_AnswerCall.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_AnswerCall.Name = "btn_AnswerCall";
+            this.btn_AnswerCall.Size = new System.Drawing.Size(107, 32);
+            this.btn_AnswerCall.TabIndex = 8;
+            this.btn_AnswerCall.Text = "전화 받기";
+            this.btn_AnswerCall.UseVisualStyleBackColor = true;
+            this.btn_AnswerCall.Click += new System.EventHandler(this.btn_AnswerCall_Click);
+            // 
+            // btn_ClearCall
+            // 
+            this.btn_ClearCall.Location = new System.Drawing.Point(429, 179);
+            this.btn_ClearCall.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_ClearCall.Name = "btn_ClearCall";
+            this.btn_ClearCall.Size = new System.Drawing.Size(107, 32);
+            this.btn_ClearCall.TabIndex = 9;
+            this.btn_ClearCall.Text = "전화 끊기";
+            this.btn_ClearCall.UseVisualStyleBackColor = true;
+            this.btn_ClearCall.Click += new System.EventHandler(this.btn_ClearCall_Click);
+            // 
+            // txt_AgentDn
+            // 
+            this.txt_AgentDn.Location = new System.Drawing.Point(135, 83);
+            this.txt_AgentDn.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_AgentDn.Name = "txt_AgentDn";
+            this.txt_AgentDn.Size = new System.Drawing.Size(284, 28);
+            this.txt_AgentDn.TabIndex = 11;
+            // 
+            // lbl_AgentDn
+            // 
+            this.lbl_AgentDn.AutoSize = true;
+            this.lbl_AgentDn.Location = new System.Drawing.Point(59, 87);
+            this.lbl_AgentDn.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_AgentDn.Name = "lbl_AgentDn";
+            this.lbl_AgentDn.Size = new System.Drawing.Size(31, 18);
+            this.lbl_AgentDn.TabIndex = 10;
+            this.lbl_AgentDn.Text = "DN";
+            // 
+            // txt_DestDn
+            // 
+            this.txt_DestDn.Location = new System.Drawing.Point(135, 223);
+            this.txt_DestDn.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_DestDn.Name = "txt_DestDn";
+            this.txt_DestDn.PasswordChar = '*';
+            this.txt_DestDn.Size = new System.Drawing.Size(284, 28);
+            this.txt_DestDn.TabIndex = 13;
+            // 
+            // lbl_DestDn
+            // 
+            this.lbl_DestDn.AutoSize = true;
+            this.lbl_DestDn.Location = new System.Drawing.Point(20, 228);
+            this.lbl_DestDn.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_DestDn.Name = "lbl_DestDn";
+            this.lbl_DestDn.Size = new System.Drawing.Size(73, 18);
+            this.lbl_DestDn.TabIndex = 12;
+            this.lbl_DestDn.Text = "Dest DN";
+            // 
+            // btn_MakeCall
+            // 
+            this.btn_MakeCall.Location = new System.Drawing.Point(429, 223);
+            this.btn_MakeCall.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_MakeCall.Name = "btn_MakeCall";
+            this.btn_MakeCall.Size = new System.Drawing.Size(107, 32);
+            this.btn_MakeCall.TabIndex = 14;
+            this.btn_MakeCall.Text = "전화 걸기";
+            this.btn_MakeCall.UseVisualStyleBackColor = true;
+            this.btn_MakeCall.Click += new System.EventHandler(this.btn_MakeCall_Click);
+            // 
+            // lbl_LogLabel
+            // 
+            this.lbl_LogLabel.Location = new System.Drawing.Point(-1, 309);
+            this.lbl_LogLabel.Name = "lbl_LogLabel";
+            this.lbl_LogLabel.Size = new System.Drawing.Size(555, 46);
+            this.lbl_LogLabel.TabIndex = 15;
+            this.lbl_LogLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // SoftPhoneSample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 356);
+            this.Controls.Add(this.lbl_LogLabel);
+            this.Controls.Add(this.btn_MakeCall);
+            this.Controls.Add(this.txt_DestDn);
+            this.Controls.Add(this.lbl_DestDn);
+            this.Controls.Add(this.txt_AgentDn);
+            this.Controls.Add(this.lbl_AgentDn);
+            this.Controls.Add(this.btn_ClearCall);
+            this.Controls.Add(this.btn_AnswerCall);
+            this.Controls.Add(this.txt_AnswerCallNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.txt_UserPw);
+            this.Controls.Add(this.lbl_UserPw);
+            this.Controls.Add(this.txt_UserId);
+            this.Controls.Add(this.lbl_UserId);
+            this.Controls.Add(this.iCapi);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "SoftPhoneSample";
+            this.Text = "Soft Phone Test";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SoftPhoneSample_FormClosed);
+            ((System.ComponentModel.ISupportInitialize) (this.iCapi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button btn_AnswerCall;
+        private System.Windows.Forms.Button btn_ClearCall;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Button btn_MakeCall;
+        private AxICAPILib.AxICAPI iCapi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_AgentDn;
+        private System.Windows.Forms.Label lbl_DestDn;
+        private System.Windows.Forms.Label lbl_LogLabel;
+        private System.Windows.Forms.Label lbl_UserId;
+        private System.Windows.Forms.Label lbl_UserPw;
+        private System.Windows.Forms.TextBox txt_AgentDn;
+        private System.Windows.Forms.TextBox txt_AnswerCallNumber;
+        private System.Windows.Forms.TextBox txt_DestDn;
+        private System.Windows.Forms.TextBox txt_UserId;
+        private System.Windows.Forms.TextBox txt_UserPw;
+
+        #endregion
+    }
+}
---
+++ SoftPhoneSample/SoftPhoneSample.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Windows.Forms;
+using AxICAPILib;
+
+namespace SoftPhoneSample
+{
+    public partial class SoftPhoneSample : Form
+    {
+        private int _globalHandle;
+        private string _agentDn;
+        private string _agentId;
+        private string _thisDn;
+        private string _connectionId;
+
+        private int _monitorid;
+
+        public SoftPhoneSample()
+        {
+            InitializeComponent();
+            InitializeICAPI();
+        }
+
+        private void InitializeICAPI()
+        {
+            iCapi.ICSetLogPath(
+                2,
+                "C:/Project/Logs/SoftPhoneSample",
+                "ICAPILogs.log"
+            );
+
+            //if(g_ipAddr == "127.0.0.1"){
+            //     g_ipAddr = "52.90.207.54";
+            // }
+
+            // V_CTI_RESULT = ICAPI_CTI.ICOpenServerExCv(
+            //   ""//ds_ctiData.getColumn(0,"SERVER_NM")
+            // , ""//ds_ctiData.getColumn(0,"SERVER_PW")
+            // , g_ipAddr
+            // , ds_ctiData.getColumn(0,"SERVER_IP1")
+            // , ds_ctiData.getColumn(0,"SERVER_IP2")
+            // , toNumber(ds_ctiData.getColumn(0,"PORT"))
+            // , toNumber(ds_ctiData.getColumn(0,"TIMEOUT"))
+            // , -1
+            // , "EUC-KR"
+            // );
+
+            var openResult = iCapi.ICOpenServer(
+                "FoodVille",
+                "",
+                "10.185.191.157", // TODO 상담실에서 확인 필요, 내 아이피!
+                "10.121.111.52",
+                "10.121.111.53",
+                9202,
+                10000,
+                -1
+            );
+
+            if (openResult <= 0)
+            {
+                lbl_LogLabel.Text = "ooo ooo ooo Error!! Error Code : " + openResult;
+            }
+
+            _globalHandle = openResult;
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+            var userId = txt_UserId.Text;
+            var userPw = txt_UserPw.Text;
+            var agentDn = txt_AgentDn.Text;
+
+            var log = "ooo ooo ooo UserID : " + userId;
+            log += "\nooo ooo ooo UserPW : " + userPw;
+            lbl_LogLabel.Text = log;
+
+            // V_CTI_RESULT = ICAPI_CTI.ICRegisterAddress(
+            //   toNumber(ds_ctiData.getColumn(0,"HANDLE"))
+            // , g_ctiTelNo
+            // , ds_ctiData.getColumn(0,"TENANT_NM")
+            // , ""
+            // );
+
+            var registerResult = iCapi.ICRegisterAddress(
+                _globalHandle,
+                agentDn,
+                "FoodVille",
+                0
+            );
+
+            if (registerResult != 0)
+            {
+                lbl_LogLabel.Text = "ooo ooo ooo Error!! Error Code : " + registerResult;
+            }
+
+            // V_CTI_RESULT = ICAPI_CTI.ICAgentLogin(  
+            //   toNumber(ds_ctiData.getColumn(0,"HANDLE"))
+            // , g_ctiTelNo
+            // , g_ctiId
+            // , ds_ctiData.getColumn(0,"AGENT_PW")
+            // , ds_ctiData.getColumn(0,"TENANT_NM")
+            // , "60"
+            // , 0
+            // , 0
+            // , ds_ctiData.getColumn(0,"AGENT_PW_TYPE")
+            // );
+            
+            //3. 테스트정보
+            // - 테스트 번호 : 070-4992-4804
+            // - 테스트 계정 : FDVdev01, FDVdev01
+            // - 패스워드 : 1
+            // - 개발테스트큐 : 901096
+            //
+            // 인사멘트 : 반갑습니다. 투썸플레이스입니다.고객센터는 1번,  투썸 창업센터 문의는 2번, 부서안내는 3번을 눌러주세요.처음부터 다시 듣고 싶으시면 *표를 눌러주세요.
+            //  - 메뉴 1번 선택 : 901096
+            //  - 메뉴 3번 선택 : 901096
+
+            var loginResult = iCapi.ICAgentLogin(
+                _globalHandle,
+                agentDn,
+                userId,
+                userPw,
+                "FoodVille",
+                60,
+                0,
+                0,
+                2
+            );
+
+            if (loginResult != 0)
+            {
+                lbl_LogLabel.Text = "ooo ooo ooo Login Error! Error Code : " + loginResult;
+            }
+
+            var stateResult = iCapi.ICSetAgentState(
+                _globalHandle,
+                userId,
+                "FoodVille",
+                40,
+                1,
+                0
+            );
+
+            if (stateResult != 0)
+            {
+                lbl_LogLabel.Text = "ooo ooo ooo State Change Error! Error Code : " + stateResult;
+            }
+        }
+
+        private void btn_AnswerCall_Click(object sender, EventArgs e)
+        {
+            // V_CTI_RESULT = ICAPI_CTI.ICAnswerCall(
+            //   toNumber(ds_ctiData.getColumn(0,"HANDLE"))
+            // , g_ctiTelNo
+            // , ds_ctiData.getColumn(0,"CONN_ID")
+            // , ds_ctiData.getColumn(0,"EXHANDLE")
+            // );
+
+            var agentDn = txt_AgentDn.Text;
+
+            iCapi.ICAnswerCall(
+                _globalHandle,
+                agentDn,
+                _connectionId,
+                0
+            );
+        }
+
+        private void btn_ClearCall_Click(object sender, EventArgs e)
+        {
+            // V_CTI_RESULT = ICAPI_CTI.ICClearCall(
+            //  toNumber(ds_ctiData.getColumn(0,"HANDLE"))
+            // , g_ctiTelNo
+            // , gfn_nvl(ds_ctiData.getColumn(0,"CONN_ID2"),ds_ctiData.getColumn(0,"CONN_ID"))
+            // , ds_ctiData.getColumn(0,"EXHANDLE")
+            // );
+
+            var agentDn = txt_AgentDn.Text;
+
+            iCapi.ICClearCall(
+                _globalHandle,
+                agentDn,
+                _connectionId,
+                0
+            );
+        }
+
+        private void btn_MakeCall_Click(object sender, EventArgs e)
+        {
+            //  V_CTI_RESULT = ICAPI_CTI.ICMakeCall(
+            //   toNumber(ds_ctiData.getColumn(0,"HANDLE"))
+            // , g_ctiTelNo
+            // , g_vocCallprefix + pTargetDn
+            // , pCallDn
+            // , 0
+            // , 0
+            // , ""
+            // , ""
+            // , 0
+            // , 0
+            // , 0
+            // , 0
+            // , ""
+            // , ds_ctiData.getColumn(0,"EXHANDLE")
+            // );
+
+            var vocCallPrefix = "630";
+            var targetDn = txt_DestDn.Text;
+            var agentDn = txt_AgentDn.Text;
+
+            iCapi.ICMakeCall(
+                _globalHandle,
+                agentDn,
+                vocCallPrefix + targetDn,
+                "07049924804",
+                0,
+                0,
+                "",
+                "",
+                0,
+                0,
+                0,
+                0,
+                "",
+                0
+            );
+        }
+
+        private void iCapi_Ringing(object sender, _DICAPIEvents_RingingEvent e)
+        {
+            _connectionId = e.conn_id;
+            _thisDn = e.this_dn;
+
+            txt_AnswerCallNumber.Text = e.ani_dn ?? e.other_dn;
+        }
+
+        private void iCapi_AgentLogin(object sender, _DICAPIEvents_AgentLoginEvent e)
+        {
+            _agentId = e.agent_id;
+            _agentDn = e.agent_dn;
+            _monitorid = e.monitorid;
+        }
+
+        private void iCapi_ResponseAgentLogin(object sender, _DICAPIEvents_ResponseAgentLoginEvent e)
+        {
+        }
+
+        private void iCapi_ResponseAnswerCall(object sender, _DICAPIEvents_ResponseAnswerCallEvent e)
+        {
+        }
+
+        private void SoftPhoneSample_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            iCapi.ICAgentLogout(
+                _globalHandle,
+                _agentDn,
+                _agentId,
+                "",
+                0
+            );
+
+            iCapi.ICUnregisterAddress(
+                _globalHandle,
+                _agentDn,
+                "",
+                _monitorid,
+                0
+            );
+
+            iCapi.ICCloseServer(_globalHandle);
+        }
+    }
+}
